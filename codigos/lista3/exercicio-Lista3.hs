3f74d0255c8fd30ac4dd4439794e9d2827072b0b
Henriquelay <2018103824@labgrad.inf.ufes.br>
2018-05-08 18:20:50 -0300

    Início da questão 2. Não finalizada. Ver como fazer pra mostrar dois resultados diferentes na mesma função, eficientemente.

--- codigos/lista3/exercicio-Lista3.hs
+++ codigos/lista3/exercicio-Lista3.hs
@@ -1,30 +1,26 @@
 {- Definindo o tipo e o comportamento das minhas funções -}
 type Ponto = (Float, Float)
-first :: Ponto -> Float
-second :: Ponto -> Float
-pertAoLosango :: Ponto -> Ponto -> Ponto -> Bool
-{- ----------------------------------------------------- -}
-
-{-  Definindo funções seletoras -}
-{- ---Módulos --- -}
-first (x,y) = x
-second (x,y) = y
-maior a b   | a >= b = a
-            | b > a = b
-menor a b   | a <= b = a
-            | b < a = b
-{- checa se o valor x está no intervalo incluso de a e b -}
-intervaloIn x a b = (x <= maior a b) && (x >= menor a b)
-intervaloEx x a b = (x < maior a b) && (x > menor a b)  
+--type Stringfloat  (String, Float)
+--distP2a2 :: Ponto -> Ponto -> Ponto -> String
 {- ----------------------------------------------------- -}
 
 {- questão 1 I -}
 pertAoRetangulo (x,y) (dx,dy) (ex,ey) = intervaloIn x dx ex && intervaloIn y dy ey
 
+{- checa se o valor x está no intervalo incluso de a e b -}
+intervaloIn x a b = (x <= maior a b) && (x >= menor a b)
+intervaloEx x a b = (x < maior a b) && (x > menor a b)
+maior a b   
+        | a >= b = a
+        | b > a = b
+menor a b
+        | a <= b = a
+        | b < a = b
+{- ----------------------------------------------------- -}
+
 {- questão 1 II -}
 pertAoLosango (x,y) (ex,ey) (sx,sy) = (entreAsRetas (x,y) (ex,ey) (sx,sy) (ex,ey) (sx,sy-2*ey)) && (entreAsRetas (x,y) (ex+2*sx,ey) (sx,sy) (ex+2*sx,ey) (sx,sy-2*ey))
 
-{- ---------------------------------------------------- -}
 {- Definindo formulas gerais de retas dados dois pontos -}
 coefAngular (x1,y1) (x2,y2) = (y1-y2)/(x1-x2)
 coefLinear (x1,y1) (x2,y2) = y1 - coefAngular (x1,y1) (x2,y2) * x1
@@ -37,9 +33,13 @@ entreAsRetas (x,y) (xP1,yP1) (xP2,yP2) (xp3,yp3) (xp4,yp4) = abaixoDaReta (x,y) 
 {- ---------------------------------------------------- -}
 
 {- questão 1 III -}
-pertAoCirculo (x,y) (cx,cy) r = if distP (x,y) (cx,cy) <= r
-                                then "Pertence ao circulo"
-                                else "Nao pertence ao circulo"
+pertAoCirculo (x,y) (cx,cy) r   
+                            | distP (x,y) (cx,cy) <= r = "Pertence ao circulo"
+                            | otherwise = "Nao pertence ao circulo"
 
 {- distancia entre dois pontos qualquer -}
-distP (x1,y1) (x2,y2) = sqrt((x1-x2)**2 + (y1-y2)**2 ) 
+distP (x1,y1) (x2,y2) = sqrt((x1-x2)**2 + (y1-y2)**2 )
+{- --------------------------------------------------- -}
+
+{- Questão 2 -}
+--distP2a2 (x1,y1) (x2,y2) (x3,y3) = show(("Pontos um e dois",distP (x1,y1) (x2,y2)) ++ ("Pontos um e tres",distP (x1,y1) (x3,y3)) ++ ("Pontos dois e tres",distP (x2,y2) (x3,y3)))
