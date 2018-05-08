a25094aa84c9385f8ac5b04d26773e3aee0e4da1
henriquelay <henriquelayber@gmail.com>
2018-05-07 23:30:21 -0300

    CARALHO CONSEGUI A QUESTAO DO LOSANGO DNAUIfBHDAISJUDASND

--- codigos/lista3/exercicio-Lista3.hs
+++ codigos/lista3/exercicio-Lista3.hs
@@ -2,7 +2,7 @@
 type Ponto = (Float, Float)
 first :: Ponto -> Float
 second :: Ponto -> Float
-pertaolosango :: Ponto -> Ponto -> Ponto -> Bool
+pertAoLosango :: Ponto -> Ponto -> Ponto -> Bool
 {-                                                       -}
 
 {-  Definindo funções seletoras -}
@@ -14,16 +14,23 @@ maior a b   | a >= b = a
 menor a b   | a <= b = a
             | b < a = b
 {- checa se o valor x está no intervalo incluso de a e b -}
-intervaloin x a b = (x <= maior a b) && (x >= menor a b)
-intervaloex x a b = (x < maior a b) && (x > menor a b)  
-
-{- angulo de uma reta sabendo dois pontos -}
-anguloreta (x1,y1) (x2,y2) = (y2-y1)/(x2-x1)
-ynox (x1,y1) (x2,y2) = (anguloreta (x1,y1) (x2,y2))*(x2-x1)+y1
+intervaloIn x a b = (x <= maior a b) && (x >= menor a b)
+intervaloEx x a b = (x < maior a b) && (x > menor a b)  
 {-                                                       -}
 
 {- questão 1 I -}
-pertaoretangulo (x,y) (dx,dy) (ex,ey) = intervaloin x dx ex && intervaloin y dy ey
+pertAoRetangulo (x,y) (dx,dy) (ex,ey) = intervaloIn x dx ex && intervaloIn y dy ey
 
 {- questão 1 II -}
-pertaolosango (x,y) (ex,ey) (sx,sy) = (y <= ynox (ex,ey) (sx,sy) && y >= ynox (ex,ey) (sx,(sy-2*ey)) && (y <= ynox ((ex+2*sx),ey) (sx,sy) && y >= ynox ((ex+2*sx),ey) (sx,(sy-2*ey)))
+pertAoLosango (x,y) (ex,ey) (sx,sy) = (entreAsRetas (x,y) (ex,ey) (sx,sy) (ex,ey) (sx,sy-2*ey)) && (entreAsRetas (x,y) (ex+2*sx,ey) (sx,sy) (ex+2*sx,ey) (sx,sy-2*ey))
+
+{- Definindo formulas gerais de retas dados dois pontos -}
+coefAngular (x1,y1) (x2,y2) = (y1-y2)/(x1-x2)
+coefLinear (x1,y1) (x2,y2) = y1 - coefAngular (x1,y1) (x2,y2) * x1
+imagemNoX xp (x1,y1) (x2,y2) = coefAngular (x1,y1) (x2,y2) * xp + coefLinear (x1,y1) (x2,y2)
+
+{- Definindo relações de um ponto X para uma reta genérica -}
+acimaDaReta (x,y) (xp1,yp1) (xp2,yp2) = y >= imagemNoX x (xp1,yp1) (xp2,yp2)
+abaixoDaReta (x,y) (xp1,yp1) (xp2,yp2) = y <= imagemNoX x (xp1,yp1) (xp2,yp2)
+{- Note que xP1 é a reta maior e portanto P está em maiúsculo -}
+entreAsRetas (x,y) (xP1,yP1) (xP2,yP2) (xp3,yp3) (xp4,yp4) = abaixoDaReta (x,y) (xP1,yP1) (xP2,yP2) && acimaDaReta (x,y) (xp3,yp3) (xp4,yp4)
