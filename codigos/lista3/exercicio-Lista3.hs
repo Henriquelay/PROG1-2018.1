0465bda90d83545acc6a36c5d463fc025753c131
henriquelay <henriquelayber@gmail.com>
2018-05-09 15:52:18 -0300

    Trouxe as equações modulares de 1 II para o where, promovendo a leitura

--- codigos/lista3/exercicio-Lista3.hs
+++ codigos/lista3/exercicio-Lista3.hs
@@ -18,17 +18,15 @@ menor a b
 
 {- questão 1 II -}
 pertAoLosango (x,y) (ex,ey) (sx,sy) = (entreAsRetas (x,y) (ex,ey) (sx,sy) (ex,ey) (sx,sy-2*ey)) && (entreAsRetas (x,y) (ex+2*sx,ey) (sx,sy) (ex+2*sx,ey) (sx,sy-2*ey))
-
-{- Definindo formulas gerais de retas dados dois pontos -}
-coefAngular (x1,y1) (x2,y2) = (y1-y2)/(x1-x2)
-coefLinear (x1,y1) (x2,y2) = y1 - coefAngular (x1,y1) (x2,y2) * x1
-imagemNoX xp (x1,y1) (x2,y2) = coefAngular (x1,y1) (x2,y2) * xp + coefLinear (x1,y1) (x2,y2)
-{- Definindo relações de um ponto X para uma reta genérica -}
-acimaDaReta (x,y) (xp1,yp1) (xp2,yp2) = y >= imagemNoX x (xp1,yp1) (xp2,yp2)
-abaixoDaReta (x,y) (xp1,yp1) (xp2,yp2) = y <= imagemNoX x (xp1,yp1) (xp2,yp2)
-{- Note que xP1 é a reta maior e portanto P está em maiúsculo -}
-entreAsRetas (x,y) (xP1,yP1) (xP2,yP2) (xp3,yp3) (xp4,yp4) = abaixoDaReta (x,y) (xP1,yP1) (xP2,yP2) && acimaDaReta (x,y) (xp3,yp3) (xp4,yp4)
-{- ---------------------------------------------------- -}
+                                        where {- Definindo formulas gerais de retas dados dois pontos -}
+                                                coefAngular (x1,y1) (x2,y2) = (y1-y2)/(x1-x2)
+                                                coefLinear (x1,y1) (x2,y2) = y1 - coefAngular (x1,y1) (x2,y2) * x1
+                                                imagemNoX xp (x1,y1) (x2,y2) = coefAngular (x1,y1) (x2,y2) * xp + coefLinear (x1,y1) (x2,y2)
+                                                {- Definindo relações de um ponto X para uma reta genérica -}
+                                                acimaDaReta (x,y) (xp1,yp1) (xp2,yp2) = y >= imagemNoX x (xp1,yp1) (xp2,yp2)
+                                                abaixoDaReta (x,y) (xp1,yp1) (xp2,yp2) = y <= imagemNoX x (xp1,yp1) (xp2,yp2)
+                                                {- Note que xP1 é a reta maior e portanto P está em maiúsculo -}
+                                                entreAsRetas (x,y) (xP1,yP1) (xP2,yP2) (xp3,yp3) (xp4,yp4) = abaixoDaReta (x,y) (xP1,yP1) (xP2,yP2) && acimaDaReta (x,y) (xp3,yp3) (xp4,yp4)
 
 {- questão 1 III -}
 pertAoCirculo (x,y) (cx,cy) r   
